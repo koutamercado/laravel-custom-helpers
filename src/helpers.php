9dc32c77fd834b84de5665e24e776fb211419d43
Carlos Mercado <koutamercado@gmail.com>
2018-08-03 19:33:10 -0300

    feature: ip_in_range-1
    Se agregó una nueva función que permite verificar si una IP está dentro de un rango IP.

--- src/helpers.php
+++ src/helpers.php
@@ -16,3 +16,25 @@ if (! function_exists('ddd')) {
         }
     }
 }
+
+if (! function_exists('ipInRange')) {
+    /**
+     * Verifico si una IP esta dentro de una rango de ip
+     *
+     * @param  string  $ip
+     * @param  string  $range
+     * @return boolean
+     */
+    function ipInRange($ip, $range)
+    {
+        if (strpos($range, '/') == false) {
+            $range .= '/32';
+        }
+        list( $range, $netmask ) = explode('/', $range, 2);
+        $range_decimal = ip2long($range);
+        $ip_decimal = ip2long($ip);
+        $wildcard_decimal = pow(2, ( 32 - $netmask )) - 1;
+        $netmask_decimal = ~ $wildcard_decimal;
+        return ( ( $ip_decimal & $netmask_decimal ) == ( $range_decimal & $netmask_decimal ) );
+    }
+}
